4fdbecdae2b7cfc0eab92399076ac61c555bc378
end-4 <97237370+end-4@users.noreply.github.com>
2023-10-03 15:01:14 +0700

    Create YOU_SHOULD_NOT_CLONE_THIS_BRANCH.sh

---
+++ YOU_SHOULD_NOT_CLONE_THIS_BRANCH.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo 'If you cloned the main branch, you did it wrong. 
+Please read the readme and clone the correct branch.
+Installation has to be done manually. 
+Sorry for the inconvenience but I am too lazy'
